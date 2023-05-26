1cce653c5f3fe806946b156c6ec477b717d43ba5
Gabriel-Triceri <rm97121@fiap.com.br>
2023-05-26 14:47:39 -0300

    Alteração BD

--- Alimenta-AI-BD/Alimenta-AI.sql
+++ Alimenta-AI-BD/Alimenta-AI.sql
@@ -9,7 +9,7 @@ CREATE TABLE cliente (
 );
 
 CREATE TABLE usuario (
-  clienteId CHAR(20) PRIMARY KEY,
+  clienteId CHAR(20) PRIMARY KEY, 
   cpf CHAR(11) UNIQUE NOT NULL,
   nascimento varchar2(8) NOT NULL, --DDmmYYYY
   FOREIGN KEY (clienteId) REFERENCES cliente(clienteId)
@@ -20,3 +20,16 @@ CREATE TABLE instituicao (
   cnpj CHAR(14) UNIQUE NOT NULL,
   FOREIGN KEY (clienteId) REFERENCES cliente(clienteId)
 );
+
+CREATE TABLE alimento (
+  alimentoId CHAR(20) PRIMARY KEY,
+  nome VARCHAR2(50) NOT NULL,
+  descricao VARCHAR2(200),
+  validade DATE,
+  quantidade NUMBER(10, 2) NOT NULL,
+  clienteId CHAR(20) NOT NULL,
+  FOREIGN KEY (clienteId) REFERENCES cliente(clienteId)
+);
+
+
+
